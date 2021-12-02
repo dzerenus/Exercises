d21816d32089e02e86ded290ad378bfbc24b95c3
ShpriZZ <Dzerenus@ya.ru>
2021-12-02 09:43:59 +0900

    Забелин — Лабораторная работа №4
    - Добавлены комментарии к коду.
    - Небольшие исправлений методов и функций.

--- Zabelin_VMK20/L04/Program.cs
+++ Zabelin_VMK20/L04/Program.cs
@@ -1,18 +1,16 @@
-﻿using System;
-
-namespace L04
+﻿namespace L04
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Получаем размер матрицы.
-            Console.Write("  Size: ");
-            var rank = int.Parse(Console.ReadLine());
+            System.Console.Write("  Size: ");
+            var rank = int.Parse(System.Console.ReadLine());
 
             // Создаём матрицу и выводим результат.
             var mat = new TDMatrix(rank);
-            mat.PrintResult();
+            mat.Print();
         }
     }
 }
--- Zabelin_VMK20/L04/TDMatrix.cs
+++ Zabelin_VMK20/L04/TDMatrix.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L04
 {
@@ -11,33 +7,43 @@ namespace L04
     /// </summary>
     class TDMatrix
     {
-        int size;
-        double[,] mat;
-        double[] a, b, c, alfa, beta, x, f;
+        int size;                           // Размерность матрицы
+        double[,] mat;                      // Матрица.
+        double[] a, b, c, alfa, beta, x, f; // Вспомогательные массивы.
 
+        /// <summary>
+        /// Трёхдиагональная матрица задаётся размерностью её размерностью.
+        /// Матрица заполняется случайными числами.
+        /// </summary>
+        /// <param name="rank">Размерность матрицы.</param>
         public TDMatrix(int rank)
         {
-            size = rank;
+            // Сохраняем размерность матрицы.
+            size = rank; 
 
-            mat = new double[rank, rank];
-            a = new double[rank];
-            b = new double[rank];
-            c = new double[rank];
-            alfa = new double[rank];
-            beta = new double[rank];
-            x = new double[rank];
-            f = new double[rank];
+            // Определяем все объявленные массивы.
+            mat = new double[rank, rank]; // Матрица.
+            a = new double[rank];         // Коэффициенты A.
+            b = new double[rank];         // Коэффициенты B.
+            c = new double[rank];         // Коэффициенты C.
+            alfa = new double[rank];      // Массив под Alpha-числа.
+            beta = new double[rank];      // Массив под Beta-числа.
+            x = new double[rank];         // Массив под овтеты.
+            f = new double[rank];         // Правая часть матрицы.
 
             Random rnd = new Random();
 
             for (int i = 0; i < rank; i++)
             {
+                // Заполнение диагоналей матрицы случайными числами.
+                // Заполнение массивов коэффициентов этими же числами.
                 if (i > 0) a[i] = mat[i, i - 1] = rnd.Next(-9, 9);
                 if (i < rank - 1) b[i] = mat[i, i + 1] = rnd.Next(-9, 9);
                 c[i] = mat[i, i] = rnd.Next(-9, 9);
 
                 f[i] = rnd.Next(-9, 9);
 
+                // Рассчёт alpha и beta, начиная со второй итерации.
                 if (i > 0)
                 {
                     int p = i - 1;
@@ -46,15 +52,21 @@ namespace L04
                 }
             }
 
+            // Рассчёт последнего X по последней строке матрицы.
             int r = rank - 1;
             x[r] = (f[r] - a[r] * beta[r]) / (c[r] + a[r] * alfa[r]);
 
+            // Рассчёт остальных значений X.
             for (int i = r - 1; i >= 0; i--)
                 x[i] = x[i + 1] * alfa[i + 1] + beta[i + 1];
         }
 
-        public void PrintResult()
+        /// <summary>
+        /// Процедура вывода всей информации о матрице и её решения на экран.
+        /// </summary>
+        public void Print() 
         {
+            // Вывод самой матрицы.
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++) PrintNum(mat[i, j]);
@@ -63,27 +75,33 @@ namespace L04
                 Console.WriteLine();
             }
 
+            // Вывод A-коэффициентов.
             Console.WriteLine();
             Console.Write("  a:");
             for (int i = 0; i < size; i++) PrintNum(a[i]);
 
+            // Вывод C-коэффициентов.
             Console.WriteLine();
             Console.Write("  c:");
             for (int i = 0; i < size; i++) PrintNum(c[i]);
 
+            // Вывод B-коэффициентов.
             Console.WriteLine();
             Console.Write("  b:");
             for (int i = 0; i < size; i++) PrintNum(b[i]);
 
+            // Вывод alpha чисел.
             Console.WriteLine();
             Console.WriteLine();
             Console.Write("  alfa:");
             for (int i = 0; i < size; i++) PrintNum(alfa[i]);
 
+            // Вывод beta чисел.
             Console.WriteLine();
             Console.Write("  beta:");
             for (int i = 0; i < size; i++) PrintNum(beta[i]);
 
+            // Вывод решений на экран.
             Console.WriteLine();
             Console.WriteLine();
             Console.Write("  x:");
@@ -91,6 +109,9 @@ namespace L04
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Вывод каждого числа в зависимости от его знака в виде сетки.
+        /// </summary>
         void PrintNum(double num)
         {
             if (num < 0) Console.Write(" ");
