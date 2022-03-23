c97e56fcc9f1d85bf98a3bbd94ea8821d2d0ef09
ShpriZZ <48196899+a-ShpriZZ-a@users.noreply.github.com>
2022-03-24 00:04:55 +0900

    NumMath Lab11

---
+++ NumMath_VMK20/L11/NFunction.cs
@@ -0,0 +1,116 @@
+﻿
+namespace L11;
+
+public class NFunction
+{
+    // Точность вычисления.
+    public readonly static double EPSILLON = 0.001;
+
+    public delegate double F(double x); // Делегирующая функция.
+    public readonly F Function;         // Делегат функции.
+    public readonly F DFunction;        // Делегат первой производной.
+    public readonly F DDFunction;       // Делегат второй производной.
+
+    public readonly (double A, double B) Borders; // Примерные границы корня функции.
+
+    /// <summary>
+    /// Функция поиска корня функции двумя методами.
+    /// </summary>
+    /// <param name="Func">Делегат функции.</param>
+    /// <param name="DFunc">Делегат первой производной.</param>
+    /// <param name="DDFunc">Делегат второй производной.</param>
+    public NFunction(F Func, F DFunc, F DDFunc)
+    {
+        // Назначаем переменные.
+        Function = Func;
+        DFunction = DFunc;
+        DDFunction = DDFunc;
+
+        // Ищем примерные границы функции.
+        Borders = FindBorders(-100, 100, 0.5);
+    }
+
+    /// <summary>
+    /// Поиск корня методом хорд и касательных.
+    /// </summary>
+    /// <returns>Корень функции./returns>
+    /// <exception cref="Exception"></exception>
+    public double RootByCTangents()
+    {
+        // Границы.
+        double a = Borders.A;
+        double b = Borders.B;
+
+        // Цикл работает, пока точность недостаточна.
+        while (b - a > 2 * EPSILLON)
+        {
+            double c = (a * Function(b) - b * Function(a)) / (Function(b) - Function(a));
+
+            if (Function(a) * DDFunction(a) > 0)
+            {
+                a -= Function(a) / DFunction(a);
+                b = c;
+            }
+
+            else if (Function(b) * DDFunction(b) > 0)
+            {
+                a = c;
+                b -= Function(b) / DFunction(b);
+            }
+
+            // Чтобы избежать зацикливания установим исключения. На всякий случай.
+            else throw new Exception("CYCLE ERROR");
+        }
+
+        // Возвращаем результат.
+        return (a + b) / 2;
+    }
+
+    /// <summary>
+    /// Получение корня уравнения методом половинного деления.
+    /// </summary>
+    /// <returns>Корень функции.</returns>
+    public double RootByHalfs()
+    {
+        // Границы.
+        double a = Borders.A;
+        double b = Borders.B;
+
+        while (b - a > 2 * EPSILLON)
+        {
+            // Ищем центр отрезка.
+            double c = (a + b) / 2;
+
+            // Устанавливаем новые границы.
+            if (Function(a) * Function(c) < 0) b = c;
+            else if (Function(b) * Function(c) < 0) a = c;
+            else return (a + b) / 2;
+        }
+
+        // Возвращаем результат.
+        return (a + b) / 2;
+    }
+
+    /// <summary>
+    /// Поиск примерных границ местоположения ОДНОГО корня функции.
+    /// </summary>
+    /// <param name="low">Минимальная граница поиска.</param>
+    /// <param name="max">Максимальная граница поиска.</param>
+    /// <param name="step">Шаг поиска.</param>
+    /// <returns>Кортеж из двух чисел (X1, X2) между которыми находится корень.</returns>
+    /// <exception cref="Exception">Если корень не найден.</exception>
+    private (double, double) FindBorders(double low, double max, double step)
+    {
+        // Ищем границы.
+        for (double i = low; i < max; i += step)
+        {
+            double b = i;
+            double a = i - step;
+
+            if (Function(a) * Function(b) < 0) return (a, b);
+        }
+
+        // Если не нашли, вызываем исключение.
+        throw new Exception("FUNCTION ERROR");
+    }
+}
---
+++ NumMath_VMK20/L11/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace L11;
+
+static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Лабораторная работа №11 - Вариант №8");
+
+        Console.WriteLine("\nЗадание №1");
+        Console.WriteLine("7x + 5^x - 2 = 0");
+
+        var nf1 = new NFunction(
+            delegate (double x) { return 7*x + Math.Pow(5,x) - 2; },                   // Функция.
+            delegate (double x) { return Math.Pow(5,x) * Math.Log(5) + 7; },           // Первая производная.
+            delegate (double x) { return Math.Pow(5, x) * Math.Pow(Math.Log(5), 2); }  // Вторая производная.
+            );
+
+        Console.WriteLine($"Найденные границы: {nf1.Borders.A}:{nf1.Borders.B}");
+        Console.WriteLine($"Поиск по касательным: {nf1.RootByCTangents()}");
+        Console.WriteLine($"Поиск половинным делением: {nf1.RootByHalfs()}");
+
+
+        Console.WriteLine("\nЗадание №2");
+        Console.WriteLine("-x^3 - 3x^2 - 5x + 4 = 0");
+
+        var nf2 = new NFunction(
+            delegate (double x) { return -Math.Pow(x, 3) - 3 * Math.Pow(x, 2) - 5 * x + 4; },  // Функция.
+            delegate (double x) { return -3*Math.Pow(x, 2) - 6*x - 5; },                       // Первая производная.
+            delegate (double x) { return -6*x-6; }  // Вторая производная.
+            );
+
+        Console.WriteLine($"Найденные границы: {nf2.Borders.A}:{nf2.Borders.B}");
+        Console.WriteLine($"Поиск по касательным: {nf2.RootByCTangents()}");
+        Console.WriteLine($"Поиск половинным делением: {nf2.RootByHalfs()}");
+    }
+}
