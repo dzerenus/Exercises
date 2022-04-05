f0978edf64e2603c1bb4ad71d92264bab936f187
ShpriZZ <48196899+a-ShpriZZ-a@users.noreply.github.com>
2022-04-06 01:49:46 +0900

    ф

---
+++ SAOD_VMK20/Saods/Lab06/Finds.cs
@@ -0,0 +1,56 @@
+﻿namespace Lab06;
+
+public static class Finds
+{
+    /// <summary>
+    /// Линейный поиск.
+    /// </summary>
+    /// <param name="arr">Массив.</param>
+    /// <param name="val">Искомое значение.</param>
+    /// <returns>Индекс результата.</returns>
+    public static int Linear(int[] arr, int val)
+    {
+        for (int i = 0; i < arr.Length; i++)
+            if (arr[i] == val) return i;
+
+        return -1;
+    }
+
+    /// <summary>
+    /// Бинарный поиск в массиве.
+    /// </summary>
+    /// <param name="arr">Массив.</param>
+    /// <param name="val">Искомое значение.</param>
+    /// <returns>Индекс результата.</returns>
+    public static int Binary(int[] arr, int val)
+    {
+        // Если массив пустой, выходим.
+        if (arr.Length == 0) return -1;
+
+        Array.Sort(arr); // Сортируем массив средставми языка.
+
+        // Указываем начальные границы поиска.
+        var l = 0;
+        var r = arr.Length - 1;
+
+        while (true)
+        {
+            // Максимально сужаем границы поиска.
+
+            if (r - l == 1)
+            {
+                if (arr[l] == val) return l; // Если левая граница равна искомому, возвращаем её.
+                if (arr[r] == val) return r; // Если правая граница равна искомому, возвращаем её.
+                return -1;                   // Выходим, если не нашли.
+            } 
+
+            else
+            {
+                var c = l + (r - l) / 2; // Берем центральное значение.
+                if (c == val) return c;  // Если центр равен искомому, возращаем его.
+                if (val > c) l = c;      // Если искомое больше середины, двигаем левую границу.
+                if (val < c) r = c;      // Если искомое меньше середины, двигаем правую границу.
+            }
+        }
+    }
+}
---
+++ SAOD_VMK20/Saods/Lab06/Program.cs
@@ -0,0 +1,21 @@
+﻿using Lab06;
+
+namespace Lab06
+{
+    public static class Program
+    {
+        delegate int Alg(int[] arr, int val);
+
+
+        public static void Main()
+        {
+            Alg Linear = Finds.Linear(a, 9);
+            Alg Binary = Finds.Binary(a)
+
+            var a = new int[] { 5, 4, 3, 9, 7, 2 };
+            Console.WriteLine(Finds.Linear(a, 9));
+            Console.WriteLine(Finds.Binary(a, 9));
+        }
+    }
+}
+
