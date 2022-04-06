b603656f521b4f7b6f8c9e05cff45d6579dd48a6
ShpriZZ <48196899+a-ShpriZZ-a@users.noreply.github.com>
2022-04-06 23:00:43 +0900

    b

---
+++ NumMath_VMK20/L13/Algs.cs
@@ -0,0 +1,39 @@
+﻿namespace L13;
+
+public static class Algs
+{
+    public static double Lagrange((double x, double y)[] data, double x)
+    {
+        double lagrange = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            double c = 1;
+            double z = 1;
+
+            for (int j = 0; j < data.Length; j++)
+                if (i != j)
+                {
+                    c *= x - data[i].x;
+                    z *= data[i].x - data[j].x;
+                }
+
+            lagrange += data[i].y * c / z;
+        }
+
+        return lagrange;
+    }
+
+    public static double Newton((double x, double y)[] data, double x)
+    {
+        var newton = data[^1].y;
+        double prod = 1;
+
+        for (int k = 1; k < data.Length; k++)
+        {
+            prod *= x - data[data.Length - k].x;
+            newton += data[k].y * prod;
+        }
+
+        return newton;
+    }
+}
---
+++ NumMath_VMK20/L13/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace L13;
+
+public class Program
+{
+    // Набор известных значений.
+    static (double x, double y)[] data = new (double x, double y)[] {
+        (-1.7, 7.88613),
+        (-0.7, -3.73737),
+        (0.6, 14.37696),
+        (1, 24),
+        (2, 24),
+        (3.2, 2.79552),
+    };
+
+    // X, для которого найти Y.
+    static double x = 0;
+
+    // i = 0.5
+
+    public static void Main()
+    {
+        Console.WriteLine(Algs.Lagrange(data, x));
+        Console.WriteLine(Algs.Newton(data, x));
+    }
+}
+
